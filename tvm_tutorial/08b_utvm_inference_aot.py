36bd32040c09793df8283cfeb63204c58a6fd688
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 19:00:24 +0000

    Adding tarfile member sanitization to extractall()

--- tvm_tutorial/08b_utvm_inference_aot.py
+++ tvm_tutorial/08b_utvm_inference_aot.py
@@ -124,7 +124,29 @@ tvm.micro.export_model_library_format(module, model_library_format_tar_path)
 print("Built MLF Library: ")
 with tarfile.open(model_library_format_tar_path, "r:*") as tar_f:
     print("\n".join(f" - {m.name}" for m in tar_f.getmembers()))
-    tar_f.extractall(model_library_format_tar_path.parent)
+    
+    import os
+    
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar_f, model_library_format_tar_path.parent)
 
 #repo_root = pathlib.Path(
 #    subprocess.check_output(["git", "rev-parse", "--show-toplevel"], encoding="utf-8").strip()
